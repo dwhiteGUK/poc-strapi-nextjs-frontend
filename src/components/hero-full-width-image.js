2c63679f6c29b345b31b23bb20466316ba4a2341
Darren White <dwhite@gentingcasinos.co.uk>
2021-02-20 13:46:20 +0000

    Test API URL used on client

--- src/components/hero-full-width-image.js
+++ src/components/hero-full-width-image.js
@@ -7,6 +7,7 @@ export default function HeroFullWidthImage({ data }) {
 
   return (
     <div className="relative bg-gray-50 pt-16 sm:pt-24 lg:pt-32">
+      {API_URL}
       <div className="mx-auto max-w-md px-4 text-center sm:px-6 sm:max-w-3xl lg:px-8 lg:max-w-7xl">
         <div>
           <h2 className="text-base font-semibold tracking-wider text-cyan-600 uppercase">{data.Intro.Sub_heading}</h2>
