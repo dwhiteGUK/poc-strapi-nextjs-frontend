6177a5da039bcdccbb372f16355460b8de23efbe
Darren White <dwhite@gentingcasinos.co.uk>
2021-02-22 10:44:42 +0000

    :sparkles: news details page

--- src/components/news-card.js
+++ src/components/news-card.js
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-export default function NewsCaed({ item }) {
+export default function NewsCard({ item }) {
 
   return (
     <div className="flex flex-col rounded-lg shadow-lg overflow-hidden">
@@ -9,11 +9,11 @@ export default function NewsCaed({ item }) {
       <div className="flex-1 bg-white p-6 flex flex-col justify-between">
         <div className="flex-1">
           <p className="text-sm font-medium text-cyan-600">
-            <a href="#" className="hover:underline">
+            <a href={`/news/${item.id}`} className="hover:underline">
               {item.news_category.Category}
             </a>
           </p>
-          <a href="#" className="block mt-2">
+          <a href={`/news/${item.id}`} className="block mt-2">
             <p className="text-xl font-semibold text-gray-900">
               {item.Heading}
             </p>
@@ -24,14 +24,14 @@ export default function NewsCaed({ item }) {
         </div>
         <div className="mt-6 flex items-center">
           <div className="flex-shrink-0">
-            <a href="#">
+            <a href={`/news/${item.id}`}>
               <span className="sr-only">Roel Aufderehar</span>
               <img className="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixqx=UsVmjgUMfb&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
             </a>
           </div>
           <div className="ml-3">
             <p className="text-sm font-medium text-gray-900">
-              <a href="#" className="hover:underline">Roel Aufderehar</a>
+              <a href={`/news/${item.id}`} className="hover:underline">Roel Aufderehar</a>
             </p>
             <div className="flex space-x-1 text-sm text-gray-500">
               <time dateTime="2020-03-16">Mar 16, 2020</time>
--- src/lib/news.js
+++ src/lib/news.js
@@ -5,4 +5,11 @@ const getNews = async () => {
   return data
 }
 
-export { getNews }
+const getNewsItem = async (id) => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/news-items/${id}`)
+  const data = await res.json()
+
+  return data
+}
+
+export { getNews, getNewsItem }
