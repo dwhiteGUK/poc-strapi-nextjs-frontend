51799ea77e77ae57deed9dac82e1bf2076cc7cb7
Darren White <dwhite@gentingcasinos.co.uk>
2021-02-24 16:29:09 +0000

    :green_heart: fix build (getting static pages generation)

--- src/lib/news.js
+++ src/lib/news.js
@@ -7,7 +7,7 @@ const getNews = async () => {
 
 const getNewsItem = async (id, preview = false) => {
   // check for preview mode, add required query parameter if we are in preview mode
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/news-items/${id}${preview && '?_publicationState=preview'}`)
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/news-items/${id}${preview ? '?_publicationState=preview' : ''}`)
   const data = await res.json()
 
   return data
