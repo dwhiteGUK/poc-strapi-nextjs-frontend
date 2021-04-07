a4437f0392e9ab65bb6b1ef31dc23a6e69c7ad1e
Darren White <dwhite@gentingcasinos.co.uk>
2021-04-07 16:17:09 +0100

    :bug: fix whats on catgeory display

--- src/lib/whats-on.js
+++ src/lib/whats-on.js
@@ -2,7 +2,7 @@ function buildQueryParams({ casino, category, region, sort, page, limit }) {
   let queryStr = `_start=${page ? page * limit : 0}&_limit=${limit}&`
 
   if (casino) queryStr += `casinos.id=${casino}&`
-  if (category) queryStr += `whats_on_category.id=${category}&`
+  if (category) queryStr += `category.id=${category}&`
   if (region) queryStr += `regions.id=${region}&`
 
   if (sort) queryStr += `_sort=${sort}`
@@ -14,7 +14,7 @@ const getWhatsOn = async (casino, category, region, sort) => {
   const queryParams = buildQueryParams(casino, category, region, sort)
 
   try {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/whats-on${queryParams ? `?${queryParams}` : ''}`)
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/whats-ons${queryParams ? `?${queryParams}` : ''}`)
     const data = await res.json()
 
     return data
@@ -26,7 +26,7 @@ const getWhatsOn = async (casino, category, region, sort) => {
 const getWhatsOnCategories = async () => {
 
   try {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/whats-on-categories`)
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/categories`)
     const data = await res.json()
 
     return data
@@ -37,7 +37,7 @@ const getWhatsOnCategories = async () => {
 
 const getWhatsOnCount = async () => {
   try {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/whats-on/count`)
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/whats-ons/count`)
     const data = await res.text()
 
     return data
